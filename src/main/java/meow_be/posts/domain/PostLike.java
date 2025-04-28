d3b5d3c8538f08a3ce42a20050f93b3f6e8adbc4
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-04-28 17:15:20 +0900

    Create PostLike.java

---
+++ src/main/java/meow_be/posts/domain/PostLike.java
@@ -0,0 +1,28 @@
+package meow_be.posts.domain;
+
+import jakarta.persistence.*;
+import meow_be.users.domain.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "post_likes")
+public class PostLike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private Boolean isLiked = true;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
