6a0d7675fabf5981eaf37a4c84131cefe90dcb2d
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-04-28 17:15:14 +0900

    Create Post.java

---
+++ src/main/java/meow_be/posts/domain/Post.java
@@ -0,0 +1,55 @@
+package meow_be.posts.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import meow_be.users.domain.User;
+
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "posts")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  // 기본 생성자 보호
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user_id;
+
+    @Column(nullable = false, length = 20)
+    private String title;
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    @Column(nullable = false, length = 10)
+    private String emotion;
+
+    @Column(nullable = false, length = 10)
+    private String postType;
+
+    @Column(nullable = false, length = 400)
+    private String transformedContent;
+
+    @Column(nullable = false)
+    private Boolean isDeleted = false;
+
+    @Column(nullable = false)
+    private Integer likeCount = 0;
+
+    @Column(nullable = false)
+    private Integer commentCount = 0;
+
+    @Column(nullable = false)
+    private Integer viewCount = 0;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt = LocalDateTime.now();
+}
