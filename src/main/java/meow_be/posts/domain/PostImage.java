864e66a8778bd769b9628bed690c1b9cde5825c0
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-04-28 18:32:39 +0900

    Update PostImage.java

--- src/main/java/meow_be/posts/domain/PostImage.java
+++ src/main/java/meow_be/posts/domain/PostImage.java
@@ -1,11 +1,18 @@
 package meow_be.posts.domain;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+@Entity
+@Table(name = "post_images")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  // 기본 생성자 보호
+@AllArgsConstructor
+@Builder(toBuilder = true)
 public class PostImage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
 
     @ManyToOne
     @JoinColumn(name = "post_id", nullable = false)
