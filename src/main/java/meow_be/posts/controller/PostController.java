2edca1de795f3d01e87bcbaa3b72101ea33742bf
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-04-29 15:06:50 +0900

    Update PostController.java

--- src/main/java/meow_be/posts/controller/PostController.java
+++ src/main/java/meow_be/posts/controller/PostController.java
@@ -1,16 +1,18 @@
 package meow_be.posts.controller;
 
 import lombok.RequiredArgsConstructor;
-import meow_be.posts.domain.Post;
 import meow_be.posts.dto.PostDto;
 import meow_be.posts.service.PostService;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 
 @Controller
 @RequestMapping("/posts")
@@ -18,16 +20,26 @@ import org.springframework.web.bind.annotation.*;
 public class PostController {
 
     private final PostService postService;
-    // 게시물 전체 조회
+
     @GetMapping
-    public ResponseEntity<Page<PostDto>> getPosts(
+    public ResponseEntity<PagedModel<PostDto>> getPosts(
             @RequestParam(value = "page", defaultValue = "0") int page,
             @RequestParam(value = "size", defaultValue = "10") int size) {
 
-        Pageable pageable = PageRequest.of(page, size);  // Pageable 객체 생성
-        Page<PostDto> postDtoPage = postService.getPosts(pageable);  // 게시물 조회
+        Pageable pageable = PageRequest.of(page, size);
+        Page<PostDto> postDtoPage = postService.getPosts(pageable);
 
-        return ResponseEntity.status(HttpStatus.OK).body(postDtoPage);
+        // PagedModel로 변환
+        PagedModel<PostDto> pagedModel = PagedModel.of(
+                postDtoPage.getContent(),  // PostDto 리스트
+                new PagedModel.PageMetadata(
+                        (long) postDtoPage.getSize(),   // size를 long으로 캐스팅
+                        (long) postDtoPage.getNumber(), // number를 long으로 캐스팅
+                        postDtoPage.getTotalElements(), // totalElements는 이미 long 타입
+                        (long) Math.ceil((double) postDtoPage.getTotalElements() / size) // totalPages 계산
+                )
+        );
+
+        return ResponseEntity.status(HttpStatus.OK).body(pagedModel);
     }
 }
-
