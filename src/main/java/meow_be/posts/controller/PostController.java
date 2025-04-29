5d350b44d3b31186ed6cf7b7a56253d2c75ebe23
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-04-29 15:28:53 +0900

    Update PostController.java

--- src/main/java/meow_be/posts/controller/PostController.java
+++ src/main/java/meow_be/posts/controller/PostController.java
@@ -3,14 +3,16 @@ package meow_be.posts.controller;
 import lombok.RequiredArgsConstructor;
 import meow_be.posts.dto.PostDto;
 import meow_be.posts.service.PostService;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/posts")
@@ -20,8 +22,16 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping
-    public ResponseEntity<Page<PostDto>> getPosts(Pageable pageable) {
-        Page<PostDto> posts = postService.getPosts(pageable);
-        return ResponseEntity.ok(posts);
+    public ResponseEntity<List<PostDto>> getPosts(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size) {
+
+        Pageable pageable = PageRequest.of(page, size);
+        Page<PostDto> postDtoPage = postService.getPosts(pageable);
+
+        List<PostDto> responseData = postDtoPage.getContent();
+
+        return ResponseEntity.ok(responseData);
     }
+
 }
