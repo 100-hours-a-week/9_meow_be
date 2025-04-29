39f1791f2c12fa32abc3bb63edf6af5885808d22
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-04-29 16:38:00 +0900

    Update PostRepository.java

--- src/main/java/meow_be/posts/repository/PostRepository.java
+++ src/main/java/meow_be/posts/repository/PostRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findByIsDeletedFalse(Pageable pageable);
+    Optional<Post> findByIdAndIsDeletedFalse(int id);
 }
