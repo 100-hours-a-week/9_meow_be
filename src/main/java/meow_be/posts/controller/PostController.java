bb75daac5db3dbe4e16f67995792c80d3fc9e1be
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-04-29 14:06:37 +0900

    Update PostController.java

--- src/main/java/meow_be/posts/controller/PostController.java
+++ src/main/java/meow_be/posts/controller/PostController.java
@@ -10,10 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/posts")
@@ -32,6 +29,13 @@ public class PostController {
 
         return ResponseEntity.status(HttpStatus.OK).body(postDtoPage);
     }
+
+    @PostMapping("/dummy")
+    public ResponseEntity<String> createDummyPost() {
+        postService.createDummyPost();
+        return ResponseEntity.ok("더미 게시글 생성 완료!");
+    }
+
 
 
 
