f14fb90cd6c9114a809e209f13b16f0ac5d44492
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-04-29 16:20:35 +0900

    Update PostService.java

--- src/main/java/meow_be/posts/service/PostService.java
+++ src/main/java/meow_be/posts/service/PostService.java
@@ -21,7 +21,6 @@ import java.util.stream.Collectors;
 public class PostService {
     private final PostRepository postRepository;
     private final PostMapper postMapper;  // PostMapper 주입
-
     public Page<PostDto> getPosts(Pageable pageable) {
         Page<Post> posts = postRepository.findByIsDeletedFalse(pageable);  // 삭제되지 않은 게시물만 조회
         return posts.map(postMapper::toDto);  // Post -> PostDto로 변환 (PostMapper 사용)
