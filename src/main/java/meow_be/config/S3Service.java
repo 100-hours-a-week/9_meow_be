76786fe7ba355f6681a6185a2491e2aa4140da81
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-04-30 17:11:12 +0900

    Update S3Service.java

--- src/main/java/meow_be/config/S3Service.java
+++ src/main/java/meow_be/config/S3Service.java
@@ -19,7 +19,7 @@ public class S3Service {
 
     private final AmazonS3 amazonS3;
 
-    @Value("${cloud.aws.s3.bucket}")
+    @Value("${spring.cloud.aws.s3.bucket}")
     private String bucket;
 
     public List<String> uploadImages(List<MultipartFile> imageFiles) {
