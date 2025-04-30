614e8a260c1ab595b56e00e88673d5b2fac5b3e6
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-04-30 16:08:47 +0900

    Create S3Service.java

---
+++ src/main/java/meow_be/config/S3Service.java
@@ -0,0 +1,50 @@
+package meow_be.config;
+
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public List<String> uploadImages(List<MultipartFile> imageFiles) {
+        List<String> imageUrls = new ArrayList<>();
+
+        for (MultipartFile file : imageFiles) {
+            try {
+                String originalFilename = file.getOriginalFilename();
+                String ext = originalFilename.substring(originalFilename.lastIndexOf("."));
+                String uniqueFileName = UUID.randomUUID() + ext;
+
+                InputStream inputStream = file.getInputStream();
+                ObjectMetadata metadata = new ObjectMetadata();
+                metadata.setContentLength(file.getSize());
+                metadata.setContentType(file.getContentType());
+
+                amazonS3.putObject(bucket, uniqueFileName, inputStream, metadata);
+
+                String fileUrl = amazonS3.getUrl(bucket, uniqueFileName).toString();
+                imageUrls.add(fileUrl);
+            } catch (Exception e) {
+                throw new RuntimeException("S3 이미지 업로드 중 오류 발생: " + e.getMessage());
+            }
+        }
+
+        return imageUrls;
+    }
+}
