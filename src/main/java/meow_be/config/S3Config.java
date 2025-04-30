a644b88701d02e8ccac50f21590b78b797bd2a93
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-04-30 17:11:10 +0900

    Update S3Config.java

--- src/main/java/meow_be/config/S3Config.java
+++ src/main/java/meow_be/config/S3Config.java
@@ -10,13 +10,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
-    @Value("${cloud.aws.credentials.access-key}") // application.yml 에 명시한 내용
+    @Value("${spring.cloud.aws.credentials.access-key}") // application.yml 에 명시한 내용
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${spring.cloud.aws.credentials.secret-key}")
     private String secretKey;
 
-    @Value("${cloud.aws.region.static}")
+    @Value("${spring.cloud.aws.region.static}")
     private String region;
 
     @Bean
