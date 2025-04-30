d2bc4a363366dd843ce909a7133cd49ceb018626
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-04-30 16:52:00 +0900

    Update PostMapper.java

--- src/main/java/meow_be/posts/mapper/PostMapper.java
+++ src/main/java/meow_be/posts/mapper/PostMapper.java
@@ -63,6 +63,7 @@ public class PostMapper {
                 post.getPostType(),
                 thumbnailUrl,
                 post.getCommentCount(),
+                post.getLikeCount(),
                 post.getCreatedAt(),
                 post.getUpdatedAt()
         );
