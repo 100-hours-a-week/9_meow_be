e0e6c535ff98a4cdb6251c39fdb287f344c0ad33
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-05-02 17:05:56 +0900

    Update TokenProvider.java

--- src/main/java/meow_be/login/security/TokenProvider.java
+++ src/main/java/meow_be/login/security/TokenProvider.java
@@ -2,18 +2,21 @@ package meow_be.login.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.beans.factory.annotation.Value;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Component
 public class TokenProvider {
 
-    @Value("${spring.jwt.secret-key}")
-    private String secretKey;
+
 
     private static final long TOKEN_EXPIRATION_TIME = 1000L * 60 * 60 * 24;  // 24시간
+    private static final SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+
+
 
     // JWT 토큰 생성
     public String createToken(Integer userId) {
@@ -21,7 +24,7 @@ public class TokenProvider {
                 .setSubject(userId.toString())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + TOKEN_EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS512, secretKey)
+                .signWith(secretKey)
                 .compact();
     }
 }
