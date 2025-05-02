812ddca54769647e54527acd1229b06ad7efacf4
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-05-02 16:57:58 +0900

    Update TokenRepository.java

--- src/main/java/meow_be/users/repository/TokenRepository.java
+++ src/main/java/meow_be/users/repository/TokenRepository.java
@@ -1,11 +1,10 @@
 package meow_be.users.repository;
 
 import meow_be.users.domain.Token;
-import meow_be.users.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Integer> {
-    Optional<Token> findByUser(User user);
+    Optional<Token> findByUserId(int userId);
 }
