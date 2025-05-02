cfe9a0f886e1fd882873e0f8c42ec785fae0604e
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-05-02 15:44:05 +0900

    Update UserRepository.java

--- src/main/java/meow_be/users/repository/UserRepository.java
+++ src/main/java/meow_be/users/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package meow_be.users.repository;
 
-import meow_be.posts.domain.PostImage;
 import meow_be.users.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByKakaoId(Long kakaoId);
 }
