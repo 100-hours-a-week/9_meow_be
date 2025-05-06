e55dd7ee5df709a239b402f4e9ca7ecf1963ef84
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-05-06 18:04:08 +0900

    Update PostController.java

--- src/main/java/meow_be/posts/controller/PostController.java
+++ src/main/java/meow_be/posts/controller/PostController.java
@@ -1,21 +1,20 @@
 package meow_be.posts.controller;
 
 import lombok.RequiredArgsConstructor;
-import meow_be.posts.domain.Post;
 import meow_be.posts.dto.PageResponse;
 import meow_be.posts.dto.PostDto;
 import meow_be.posts.dto.PostSummaryDto;
 import meow_be.posts.service.PostService;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.http.ResponseEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/posts")
@@ -31,8 +30,8 @@ public class PostController {
             @RequestParam(value = "page", defaultValue = "0") int page,
             @RequestParam(value = "size", defaultValue = "10") int size) {
 
-        Pageable pageable = PageRequest.of(page, size);
-        Page<PostSummaryDto> postPage = postService.getPostSummaryPage(pageable); // Page<PostSummaryDto>로 반환되도록 서비스 수정 필요
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdAt"));
+        Page<PostSummaryDto> postPage = postService.getPostSummaryPage(pageable);
 
         PageResponse<PostSummaryDto> response = new PageResponse<>(
                 postPage.getContent(),
