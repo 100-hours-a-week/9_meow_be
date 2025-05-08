64a757b6e7d4d8504b55730e795db4c0cc3179bd
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-05-08 13:32:02 +0900

    fix: 썸네일 이미지 url 저장 방식 수정

--- src/main/java/meow_be/config/S3Service.java
+++ src/main/java/meow_be/config/S3Service.java
@@ -1,6 +1,5 @@
 package meow_be.config;
 
-
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
@@ -27,6 +26,10 @@ public class S3Service {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
+    private final String s3Domain = "https://s3-an2-image-meowng.s3.ap-northeast-2.amazonaws.com";
+    private final String cloudFrontDomain = "https://ds36vr51hmfa7.cloudfront.net";
+
+    // 이미지 업로드
     public List<String> uploadImages(List<MultipartFile> imageFiles) {
         List<String> imageUrls = new ArrayList<>();
 
@@ -44,9 +47,7 @@ public class S3Service {
                 amazonS3.putObject(bucket, uniqueFileName, inputStream, metadata);
 
                 String fileUrl = amazonS3.getUrl(bucket, uniqueFileName).toString();
-                String s3Domain = "https://s3-an2-image-meowng.s3.ap-northeast-2.amazonaws.com";
-                String cloudFrontDomain = "https://ds36vr51hmfa7.cloudfront.net";
-                fileUrl=fileUrl.replace(s3Domain, cloudFrontDomain);
+                fileUrl = fileUrl.replace(s3Domain, cloudFrontDomain);  // CloudFront URL로 변환
                 imageUrls.add(fileUrl);
             } catch (Exception e) {
                 throw new RuntimeException("S3 이미지 업로드 중 오류 발생: " + e.getMessage());
@@ -75,16 +76,17 @@ public class S3Service {
             ByteArrayInputStream inputStream = new ByteArrayInputStream(thumbnailBytes);
             amazonS3.putObject(bucket, fileName, inputStream, metadata);
 
-            return amazonS3.getUrl(bucket, fileName).toString();
+            String fileUrl = amazonS3.getUrl(bucket, fileName).toString();
+            fileUrl = fileUrl.replace(s3Domain, cloudFrontDomain);
+            return fileUrl;
 
         } catch (IOException e) {
             throw new RuntimeException("썸네일 생성 또는 업로드 실패", e);
         }
     }
+
     private Optional<String> getFileExtension(String filename) {
         if (filename == null || !filename.contains(".")) return Optional.empty();
         return Optional.of(filename.substring(filename.lastIndexOf('.') + 1));
     }
-
-
 }
