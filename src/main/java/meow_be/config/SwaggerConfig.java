f09e7600b4af10152ad77d86097971c2894a0611
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-05-14 14:45:19 +0900

    fix: swagger 설정 수정

--- src/main/java/meow_be/config/SwaggerConfig.java
+++ src/main/java/meow_be/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
         info =@Info(
-                title ="meowng API 명세서 ",
+                title = "meowng API 명세서",
                 description = "meowng api 명세서 실시간 최신 버전",
                 version = "v1"
         )
@@ -23,6 +24,10 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         String securityJwtName = "JWT";
+
+        Server server = new Server();
+        server.setUrl("https://www.meowng.com/api");
+
         SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityJwtName);
         Components components = new Components()
                 .addSecuritySchemes(securityJwtName, new SecurityScheme()
@@ -33,8 +38,8 @@ public class SwaggerConfig {
                 );
 
         return new OpenAPI()
+                .addServersItem(server)
                 .addSecurityItem(securityRequirement)
                 .components(components);
-
     }
 }
