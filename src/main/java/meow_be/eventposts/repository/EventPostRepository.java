0aac67315c2eda06d4121d3517665ab6a1ae1c79
dyk000825 <kdy000825@naver.com>
2025-06-17 16:47:23 +0900

    fix: repositroy 수정

--- src/main/java/meow_be/eventposts/repository/EventPostRepository.java
+++ src/main/java/meow_be/eventposts/repository/EventPostRepository.java
@@ -1,6 +1,7 @@
 package meow_be.eventposts.repository;
 
 import meow_be.eventposts.domain.EventPost;
+import meow_be.eventposts.domain.EventWeek;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,6 @@ import java.util.List;
 
 @Repository
 public interface EventPostRepository extends JpaRepository<EventPost, Integer> {
-    List<EventPost> findAllByWeek(int week);
+    List<EventPost> findAllByEventWeek(EventWeek eventWeek);
+
 }
