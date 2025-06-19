a56539c06fbad3ad2c7806124af52b88c5c89e41
dyk000825 <kdy000825@naver.com>
2025-06-19 17:38:01 +0900

    feat: 역대 이벤트 1-3등 조회

--- src/main/java/meow_be/eventposts/repository/EventPostQueryRepository.java
+++ src/main/java/meow_be/eventposts/repository/EventPostQueryRepository.java
@@ -3,8 +3,10 @@ package meow_be.eventposts.repository;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import meow_be.eventposts.domain.QEventWeek;
 import meow_be.eventposts.dto.EventPostRankingDto;
 import meow_be.eventposts.domain.QEventPost;
+import meow_be.eventposts.dto.EventTopRankDto;
 import meow_be.users.domain.QUser;
 import org.springframework.stereotype.Repository;
 
@@ -34,6 +36,29 @@ public class EventPostQueryRepository {
                 .join(post.user, user)
                 .where(post.eventWeek.week.eq(week))
                 .orderBy(post.ranking.asc())
+                .fetch();
+    }
+    public List<EventTopRankDto> findTop3RankedPostsByWeek() {
+        QEventPost post = QEventPost.eventPost;
+        QUser user = QUser.user;
+        QEventWeek week = QEventWeek.eventWeek;
+
+        return queryFactory
+                .select(Projections.constructor(EventTopRankDto.class,
+                        week.week,
+                        post.id,
+                        post.imageUrl,
+                        user.nickname,
+                        user.profileImageUrl,
+                        user.animalType,
+                        post.likeCount,
+                        post.ranking
+                ))
+                .from(post)
+                .join(post.user, user)
+                .join(post.eventWeek, week)
+                .where(post.ranking.between(1, 3))
+                .orderBy(week.week.asc(), post.ranking.asc())
                 .fetch();
     }
 }
