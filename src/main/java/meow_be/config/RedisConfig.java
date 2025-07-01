5cff9f2f7a545d928139ec44c139f10fbdd959e1
dyk000825 <kdy000825@naver.com>
2025-07-01 14:13:59 +0900

    fix: redis 설정 수정

--- src/main/java/meow_be/config/RedisConfig.java
+++ src/main/java/meow_be/config/RedisConfig.java
@@ -21,10 +21,6 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
-    @Value("${spring.data.redis.password}")
-    private String password;
-
-
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(host, port);
