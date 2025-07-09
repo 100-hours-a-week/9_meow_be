084eda095b2e110bdc1a108d92555e0bb82b126c
dyk000825 <kdy000825@naver.com>
2025-07-09 17:31:27 +0900

    fix: 채팅 인증 수정

--- src/main/java/meow_be/config/JwtHandshakeInterceptor.java
+++ src/main/java/meow_be/config/JwtHandshakeInterceptor.java
@@ -12,6 +12,7 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -73,7 +74,7 @@ public class JwtHandshakeInterceptor implements ChannelInterceptor {
             }
         }
 
-        return message;
+        return MessageBuilder.createMessage(message.getPayload(), accessor.getMessageHeaders());
     }
 
     public static class ChatRoomFullException extends RuntimeException {
