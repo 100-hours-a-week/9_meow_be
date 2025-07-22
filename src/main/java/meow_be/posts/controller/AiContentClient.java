fdf3e67cd5ba7191543f9de666917cf3472ddaca
dyk000825 <kdy000825@naver.com>
2025-07-22 10:07:27 +0900

    fix: dev서버 ai 경로 수정

--- src/main/java/meow_be/posts/controller/AiContentClient.java
+++ src/main/java/meow_be/posts/controller/AiContentClient.java
@@ -16,7 +16,7 @@ public class AiContentClient {
     private final WebClient webClient;
 
     public AiContentClient(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl("https://aiserver.meowng.com:8000").build();
+        this.webClient = webClientBuilder.baseUrl("http://34.64.213.48:8000").build();
     }
 
     public String transformpostContent(String originalContent, String emotion, String post_type) {
